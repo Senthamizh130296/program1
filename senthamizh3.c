3f90f3895b191eb15b61b0189b5d161cca1bcee0
Senthamizh130296 <31921626+Senthamizh130296@users.noreply.github.com>
2017-09-14 16:45:25 +0530

    Create senthamizh3.c

---
+++ senthamizh3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int one, two, multiply;
+printf(" first number  ");
+scanf("%d",&one);
+printf(" second number ");
+scanf("%d",&two);
+multiply = one * two;
+printf(" %d",one,two,multiply);
+getch();
+}
